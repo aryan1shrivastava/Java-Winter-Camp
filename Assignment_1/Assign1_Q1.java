f9441cb5878466ce3edbff171af54852f09bd7eb
Aryan Shrivastava <aryan1shrivastava@gmail.com>
2023-01-18 20:53:36 +0530

    Add files via upload

---
+++ Assignment_1/Assign1_Q1.java
@@ -0,0 +1,22 @@
+public class Assign1_Q1 {
+    public static void main(String[] args) {
+        String str = "rajat";
+        System.out.println("Before Sorting:" + str);
+        int j;
+        char temp;
+        char[] chars = str.toCharArray();
+        for(int i=0; i < chars.length; i++) {
+            for(j=0; j < chars.length; j++) {
+                if(chars[j] > chars[i]) {
+                    temp = chars[i];
+                    chars[i] = chars[j];
+                    chars[j] = temp;
+                }
+            }
+        }
+        System.out.println("After Sorting:");
+        for (char aChar : chars) {
+            System.out.println(aChar);
+        }
+    }
+}
